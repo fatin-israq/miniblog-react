6033d80717adc69c2f485dd7a5cabd3a3ef67491
Fatin Israq Talha <israqq2120@gmail.com>
2025-12-11 21:28:12 +0600

    Update blog display layout for responsive design

--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -15,7 +15,7 @@ const Blogs = () => {
   return (
     <>
       <h1 className="text-3xl">Total: {blogs.length}</h1>
-      <div className="all-blogs grid grid-cols-2">
+      <div className="all-blogs grid sm:grid-cols-1 lg:grid-cols-3 justify-items-center">
         {blogs.map((blog) => (
           <Blog key={blog.id} blog={blog}></Blog>
         ))}
